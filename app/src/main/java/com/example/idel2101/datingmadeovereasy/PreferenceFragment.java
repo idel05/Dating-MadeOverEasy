5967a2df9e1754b1ece4593b6bdacd4d9e25fc02
idel05 <42978812+idel05@users.noreply.github.com>
2019-01-18 17:19:39 -0600

    Added something about inflater?idk what that is this is confusing

--- app/src/main/java/com/example/idel2101/datingmadeovereasy/PreferenceFragment.java
+++ app/src/main/java/com/example/idel2101/datingmadeovereasy/PreferenceFragment.java
@@ -1,6 +1,8 @@
 package com.example.idel2101.datingmadeovereasy;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,9 +11,11 @@ import android.view.ViewGroup;
 import java.security.PublicKey;
 import java.util.zip.Inflater;
 
-public class PreferenceFragment extends Fragment {
+public class  PreferenceFragment extends Fragment {
 
-
-
-
+    @NonNull
+    @Override
+    public LayoutInflater onGetLayoutInflater(@Nullable Bundle savedInstanceState) {
+        return super.onGetLayoutInflater(savedInstanceState);
+    }
 }
