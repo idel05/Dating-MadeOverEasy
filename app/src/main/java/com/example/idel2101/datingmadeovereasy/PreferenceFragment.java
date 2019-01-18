14817118fbb64e538fc5d8ef12b0aacbafbb9874
idel05 <42978812+idel05@users.noreply.github.com>
2019-01-18 13:07:43 -0600

    Combined the 2

---
+++ app/src/main/java/com/example/idel2101/datingmadeovereasy/PreferenceFragment.java
@@ -0,0 +1,21 @@
+package com.example.idel2101.datingmadeovereasy;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.security.PublicKey;
+import java.util.zip.Inflater;
+
+public class PreferenceFragment extends Fragment {
+
+   /* @Override
+    public View onCreateView(LayoutInflater, Inflater, ViewGroup view, Bundle bundle);
+    {
+
+              
+    }*/
+
+}
