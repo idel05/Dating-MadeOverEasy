1d5e3fbeb421ecb13c18a35e63886f192b9ee7ce
Mike Pelican <mpelican@sift.net>
2019-01-27 22:07:37 -0600

    figuring out how to connect pages to the menu

--- app/src/main/java/com/example/idel2101/datingmadeovereasy/EggApplication.java
+++ app/src/main/java/com/example/idel2101/datingmadeovereasy/EggApplication.java
@@ -5,14 +5,14 @@ import android.app.Application;
 import com.backendless.Backendless;
 
 public class EggApplication extends Application {
-    public static final String APPLICATON_ID = "DFC3BBB7-51E6-DBDF-FF95-2910EA68D800";
+    public static final String APPLICATION_ID = "DFC3BBB7-51E6-DBDF-FF95-2910EA68D800";
     public static final String API_KEY = "C23043C9-B5C5-2EBF-FFBC-158A32F24600";
     public static final String SERVER_URL = "https://api.backendless.com";
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Backendless.setUrl(SERVER_URL);
-        Backendless.initApp(getApplicationContext(), APPLICATON_ID, API_KEY);
+       Backendless.setUrl(SERVER_URL);
+        Backendless.initApp(getApplicationContext(), APPLICATION_ID, API_KEY);
     }
 }
